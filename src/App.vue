28cea0ae471df6f80094bab1a057b5ec675fc61f
AnastasiaEvl <nastya.e.1234@gmail.com>
2024-02-01 15:41:08 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,161 @@
+<script>
+import User from "@/components/User.vue";
+
+export default {
+  components: {User},
+  data() {
+    return {
+      users: [],
+      error: '',
+      userName: '',
+      userSurname: '',
+      userDepartment: '',
+      userEmail: ''
+    }
+  },
+  methods: {
+    sendData() {
+      if (this.userName === '') {
+        this.error = 'Name is empty'
+        return
+      } else if (this.userSurname === '') {
+        this.error = 'Surname is empty'
+        return
+      } else if (this.userDepartment === '') {
+        this.error = 'Department is empty'
+        return
+      } else if (this.userEmail === '') {
+        this.error = 'Email is empty'
+        return
+      }
+      this.error = ''
+      if (this.users.length === 0) {
+        this.users.push({
+          name: this.userName,
+          surname: this.userSurname,
+          department: this.userDepartment,
+          email: this.userEmail
+        })
+      } else {
+        if (this.users.find(el => el.email === this.userEmail)) {
+          this.error = 'Already exist'
+        } else {
+          this.users.push({
+            name: this.userName,
+            surname: this.userSurname,
+            department: this.userDepartment,
+            email: this.userEmail
+          })
+        }
+      }
+      this.userName = ''
+      this.userSurname = ''
+      this.userDepartment = ''
+      this.userEmail = ''
+    },
+    deleteUser(index) {
+      this.users.splice(index, 1)
+    }
+  }
+}
+
+</script>
+
+<template>
+  <h2 class="title">List of employers</h2>
+  <form class="task-form">
+    <fieldset>
+      <input type="text" v-model="userName" placeholder="Name" class="input">
+      <input type="text" v-model="userSurname" placeholder="Surname" class="input">
+      <input type="text" v-model="userDepartment" placeholder="Department" class="input">
+      <input type="email" v-model="userEmail" placeholder="Email" class="input">
+    </fieldset>
+  </form>
+  <p class="error">{{ error }}</p>
+  <button @click="sendData()" class="submit-btn">Add</button>
+  <div v-if="users.length === 0" class="text-container">
+    <p>List is empty!</p>
+  </div>
+  <table v-if="users.length !==0" class="table">
+    <tr>
+      <th>№</th>
+      <th>Name</th>
+      <th>Surname</th>
+      <th>Department</th>
+      <th>Email</th>
+    </tr>
+    <User v-for="(el, index) in users" :key="index" :user="el" :index="index" :deleteUser="deleteUser"/>
+  </table>
+</template>
+
+<style scoped>
+.title {
+  text-align: center;
+  color: white;
+  padding-top: 2rem;
+}
+.task-form {
+  margin: 3rem auto;
+  width: 80%;
+  padding: 2rem;
+  display: flex;
+  background-color: #5e83ba;
+  justify-content: center;
+}
+
+.input {
+  margin-right: 1rem;
+  padding: 0.5rem;
+  text-align: center;
+}
+
+.submit-btn {
+  color: white;
+  background-color: #091d36;
+  padding: 1rem 10rem;
+  border-radius: 0.5rem;
+  transition: 0.6s;
+  position: relative;
+  left: 39%;
+}
+
+.submit-btn:hover {
+  background-color: #394e79;
+  cursor: pointer;
+  transition: 0.6s;
+}
+
+.text-container {
+  text-align: center;
+  padding-top: 3rem;
+}
+
+h3 {
+  font-weight: lighter;
+}
+
+.error {
+  color: red;
+  text-align: center;
+}
+
+.table {
+  width: 100%;
+  margin-bottom: 20px;
+  border: 1px solid #dddddd;
+  border-collapse: collapse;
+  margin-top: 2rem;
+}
+
+.table th {
+  font-weight: bold;
+  padding: 5px;
+  background: #efefef;
+  border: 1px solid #dddddd;
+}
+
+.table td {
+  border: 1px solid #dddddd;
+  padding: 5px;
+}
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,54 @@
+<script>
+
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true
+    },
+    index: {
+      type: Number,
+      required: true
+    },
+    deleteUser: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <tr>
+    <td>{{ index + 1 }}</td>
+    <td>{{ user.name }}</td>
+    <td>{{ user.surname }}</td>
+    <td> {{ user.department }}</td>
+    <td> {{ user.email }}</td>
+    <td><button @click="deleteUser(index)" class="delete-btn">X</button></td>
+  </tr>
+
+
+</template>
+
+<style scoped>
+
+td {
+  border: 1px solid #dddddd;
+  padding: 5px;
+}
+.delete-btn{
+  position: relative;
+  height: 100%;
+  width: 100%;
+  transition: 0.6s;
+}
+.delete-btn:hover{
+  transition: 0.6s;
+  cursor: pointer;
+  background-color: #ff7b5a;
+  color:white;
+  border-color: white;
+}
+
+</style>
